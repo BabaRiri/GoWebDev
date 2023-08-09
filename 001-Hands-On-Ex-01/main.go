04825c0d881124d43db6485271bee8fab0ac9e44
unknown <sewardchard@gmail.com>
2023-08-09 14:01:53 +0300

    Review of Go language and interfaces

--- 001-Hands-On-Ex-01/main.go
+++ 001-Hands-On-Ex-01/main.go
@@ -1,13 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type square struct {
-	side int
+	side float64
 }
 
 type circle struct {
 	radius float64
 }
 
-func (s square) area() int {
+func (s square) area() float64 {
 	return s.side * 2
+}
+
+func (c circle) area() float64 {
+	return math.Pi * (c.radius * c.radius)
+}
+
+type shape interface {
+	area() float64
+}
+
+func printArea(sh shape) {
+	fmt.Printf("Area: %f\n", sh.area())
+}
+
+func main() {
+	s1 := square{
+		side: 5.0,
+	}
+
+	s2 := circle{
+		radius: 3.2,
+	}
+
+	printArea(s1)
+	printArea(s2)
 }
