19d8010b29b21548dc23e2b3496c130d0e5de75b
unknown <sewardchard@gmail.com>
2023-08-09 17:05:02 +0300

    Added some comments

--- 003-Parsing-Data/main.go
+++ 003-Parsing-Data/main.go
@@ -8,11 +8,13 @@ import (
 
 var tpl *template.Template
 
+//template.Must handles error for us
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.html"))
 }
 
 func main() {
+	//We parsed "BabaRiri" to the placeholder on our html file
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.html", "BabaRiri")
 	if err != nil {
 		log.Fatalln(err)
